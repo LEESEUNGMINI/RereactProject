4bb15660670a1343eb9e718de0919f01db1cd2fc
LEESEUNGMINI <dltmdals7295@naver.com>
2024-02-15 11:40:37 +0900

    first

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export async function expressTest() {
+  try {
+    return await fetch(
+      "https://openapi.foodsafetykorea.go.kr/api/100e982ab52b4da983f3/COOKRCP01/json/"
+    ).then((res) => res.json());
+  } catch (error) {
+    console.log(error);
+  }
+}
