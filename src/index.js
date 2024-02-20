75c95abb26b699848bbc8846ea793f76b92a5618
LEESEUNGMINI <dltmdals7295@naver.com>
2024-02-20 13:25:42 +0900

    re

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Cucun from "./routes/Cucun";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/chcun",
+    element: <Cucun />,
+  },
+]);
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/routes/Cucun.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Component/Header";
+import Footer from "../Component/Footer";
+
+export default function Cucun() {
+  return (
+    <div>
+      <Header />
+      zz
+      <Footer />
+    </div>
+  );
+}
